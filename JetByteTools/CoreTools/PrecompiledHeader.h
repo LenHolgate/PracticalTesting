0e9b932d6715d9261c311c0d6ae67b78ae17e02a
Len Holgate <len.holgate@jetbyte.com>
2023-04-15 16:18:06 +0100

    Fix missing precompiled headers

---
+++ JetByteTools/CoreTools/PrecompiledHeader.h
@@ -0,0 +1,105 @@
+#pragma once
+///////////////////////////////////////////////////////////////////////////////
+// File: PrecompiledHeader.h
+///////////////////////////////////////////////////////////////////////////////
+//
+// The code in this file is released under the The MIT License (MIT)
+//
+// Copyright (c) 2021 JetByte Limited.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the “Software”), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+// DEALINGS IN THE SOFTWARE.
+//
+///////////////////////////////////////////////////////////////////////////////
+// This file was automatically generated. Do not edit.
+///////////////////////////////////////////////////////////////////////////////
+
+// Excludes: Admin.h
+
+#include "Atomic.h"
+#include "AtomicLong.h"
+#include "AutoResetEvent.h"
+#include "CallbackTimerQueueEx.h"
+#include "CallbackTimerWheel.h"
+#include "CheckedStaticCast.h"
+#include "CompareStrings.h"
+#include "ConditionalSmartPointer.h"
+#include "ContainingRecord.h"
+#include "DebugHelpers.h"
+#include "DebugTrace.h"
+#include "DeclareDerivedClass.h"
+#include "EmptyBase.h"
+#include "ErrorCodeException.h"
+#include "ErrorCodeToErrorMessage.h"
+#include "Event.h"
+#include "Exception.h"
+#include "ExpandableBuffer.h"
+#include "FileUtils.h"
+#include "GlobalErrorHandler.h"
+#include "IListenToThreadNaming.h"
+#include "IListenToThreadStart.h"
+#include "IListenToThreadStop.h"
+#include "ILockableObject.h"
+#include "ILogMessages.h"
+#include "IManageTimerQueue.h"
+#include "IMonitorCallbackTimerQueue.h"
+#include "IMonitorThreadedCallbackTimerQueue.h"
+#include "IProvideTickCount.h"
+#include "IProvideTickCount64.h"
+#include "IQueueTimers.h"
+#include "IReentrantLockableObject.h"
+#include "IRunnable.h"
+#include "IWaitable.h"
+#include "IntrusiveMultiMap.h"
+#include "IntrusiveMultiMapNode.h"
+#include "IntrusiveRedBlackTree.h"
+#include "IntrusiveRedBlackTreeNode.h"
+#include "IntrusiveSet.h"
+#include "IntrusiveSetNode.h"
+#include "LockableObject.h"
+#include "LockableObjectOwners.h"
+#include "ManualResetEvent.h"
+#include "MessageLog.h"
+#include "NullCallbackTimerQueueMonitor.h"
+#include "NullMessageLog.h"
+#include "NullThreadedCallbackTimerQueueMonitor.h"
+#include "PerThreadErrorHandler.h"
+#include "PerformanceCounter.h"
+#include "Printf.h"
+#include "ReentrantLockableObject.h"
+#include "SimpleMessageLog.h"
+#include "StringConverter.h"
+#include "StringUtils.h"
+#include "StringVector.h"
+#include "SystemTime.h"
+#include "Thread.h"
+#include "ThreadBase.h"
+#include "ThreadId.h"
+#include "ThreadLocalStorage.h"
+#include "ThreadedCallbackTimerQueue.h"
+#include "TickCount64Provider.h"
+#include "TickCountCompare.h"
+#include "TickCountProvider.h"
+#include "ToBool.h"
+#include "ToString.h"
+#include "Types.h"
+#include "tstring.h"
+
+///////////////////////////////////////////////////////////////////////////////
+// End of file: PrecompiledHeader.h
+///////////////////////////////////////////////////////////////////////////////
